f99be2a812bc12519ec59dc64d3e9d887aada9cd
Austin <aust.crim@gmail.com>
2020-03-21 12:41:15 -0500

    begin styling

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { PostForm } from './PostForm'
+import { PostForm } from './PostForm';
+import '../styles/App.css';
 
 const App = () => {
     const [posts, setPosts] = useState([]);
@@ -9,9 +10,9 @@ const App = () => {
             .then(res => setPosts(res))
     }, []);
 
-    return <div>
-        {posts.map(post => <li key={post.title}>{post.title}</li>)}
+    return <div id="application">
         <PostForm />
+        {posts.map(post => <li key={post.title}><a href="#">{post.title}</a></li>)}
     </div>;
 };
 
--- src/client/components/PostForm.js
+++ src/client/components/PostForm.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
+import '../styles/PostForm.css';
 
 export const PostForm = () => {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
     function eventHandler(setState) {
-        return (event) => setState(event.target.value);
+        return event => setState(event.target.value);
     }
 
-    async function handleClick(e) {
-        const response = await fetch("https://austin-crim-blog-api.herokuapp.com/posts", {method: "post", body: {title: title, content: content}})
+    async function handleClick(event) {
+        console.log(event);
+        event.preventDefault();
+        const response = await fetch(
+            "https://austin-crim-blog-api.herokuapp.com/posts",
+            { method: "post", body: { title: title, content: content } }
+        );
         if (!response.ok) {
             alert("bummer");
         }
@@ -17,12 +23,21 @@ export const PostForm = () => {
 
     return (
         <div>
-            <form action="https://austin-crim-blog-api.herokuapp.com/posts" method="post">
+            <form>
                 <label>Title</label>
-                <input type="text" name="title" onChange={eventHandler(setTitle)} value={title}/>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={eventHandler(setTitle)}
+                    value={title}
+                />
                 <br />
                 <label>Content</label>
-                <textarea name="content" onChange={eventHandler(setContent)} value={content}/>
+                <textarea
+                    name="content"
+                    onChange={eventHandler(setContent)}
+                    value={content}
+                />
                 <br />
                 <button onClick={() => handleClick}>Submit</button>
             </form>
