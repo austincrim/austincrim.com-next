02f7fa9aa142d8c757b262129f36d01a83ed41cc
Austin Crim <crim.austin@principal.com>
2020-03-23 14:20:10 -0500

    refactor PostPreview, add styles, date

--- src/client/components/PostPreview.js
+++ src/client/components/PostPreview.js
@@ -1,10 +1,11 @@
 import React from "react";
-import '../styles/OutputPostPreview.css';
+import "../styles/OutputPostPreview.css";
 
-export default ({post}) => (
-    <div className="max-w-lg mx-auto flex m-10 p-6 bg-white rounded-lg shadow-md">
-        <li key={post.title}>
-            <a href="#">{post.title}</a>
-        </li>
+export default ({ post }) => (
+  <a href="#">
+    <div className="max-w-sm mx-auto flex m-10 p-6 text-center bg-white rounded-lg shadow-md hover:shadow-xl transition duration-200 ease-in-out">
+      <div className="font-black mb-3">{post.title}</div>
+      <div className="text-gray-700">{new Date(post.dateAuthored).toLocaleDateString()}</div>
     </div>
+  </a>
 );
