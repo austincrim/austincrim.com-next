3a4d197fe8203fff60ba9579b33a0e2a7d95f6dd
Austin Crim <aust.crim@gmail.com>
2020-04-06 21:37:37 -0500

    add about cards

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,24 @@ export default () => {
     return (
         <>
             <Nav />
-            About stuff
+            <div className="p-8 ml-8">
+                <h3 className="mb-12 text-4xl font-sans">About Me</h3>
+                
+                <div className="flex flex-column justify-center">
+                    <div className="p-6 mx-12 border border-blue-800 rounded shadow-md">
+                        <h4 className="text-xl font-semibold font-sans">Career</h4> 
+                        alskdjflas;kdjfl;akjdfl jasdk f;lasd kflak dss d
+                    </div>
+                    <div className="p-6 mx-12 border border-blue-800 rounded shadow-md">
+                        <h4 className="text-xl font-semibold font-sans">Education</h4>
+                        alskdjflas;kdjfl;akjdfl jasdk f;lasd kflak dss d 
+                    </div>
+                    <div className="p-6 mx-12 border border-blue-800 rounded shadow-md">
+                    <h4 className="text-xl font-semibold font-sans">Personal</h4>
+                        alskdjflas;kdjfl;akjdfl jasdk f;lasd kflak dss d 
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
