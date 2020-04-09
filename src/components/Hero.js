01b16e4a253f1de4cb361be0deac800f0c5a4233
Austin Crim <aust.crim@gmail.com>
2020-04-08 22:43:16 -0500

    hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className="w-full mx-auto my-12 px-20 py-4 bg-indigo-500 text-white shadow-md">
-            <h1 className="mb-2 text-4xl font-semibold">
-                Hi, I'm Austin.
-            </h1>
-            <h2 className="text-3xl">Welcome to my portfolio.</h2>
+        <div className="w-full mx-auto px-20 py-4 bg-gray-100 text-indigo-500">
+            <div>
+                <h1 className="mb-2 text-4xl font-semibold">
+                    Hi, I'm Austin.
+                </h1>
+                <h2 className="text-3xl font-light">Welcome to my portfolio.</h2>
+            </div>
         </div>
     );
 };
