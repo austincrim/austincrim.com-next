df9c9112cf77be5457f714bd846f603abed0a3eb
Austin <aust.crim@gmail.com>
2020-04-16 20:49:18 -0500

    padding, hamburger menu

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,7 @@ const Section = props => {
     const colorStyle = props.primary ? 'bg-gray-200 text-gray-700' : 'bg-gray-700 text-gray-300';
     
     return (
-        <div id={props.title.toLowerCase()} className={`w-full px-6 py-12 ${colorStyle}`}>
+        <div id={props.title.toLowerCase()} className={`w-full leading-loose px-8 py-12 ${colorStyle}`}>
             <div className="max-w-6xl mx-auto">
                 <h2 className={`text-3xl font-semibold mb-6 ${props.primary ? 'text-indigo-500' : 'text-indigo-400'}`}>
                     {props.title}
