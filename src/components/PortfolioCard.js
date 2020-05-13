819c9315f80a1f317fd295919e7aa88afb5deb44
Austin Crim <crim.austin@principal.com>
2020-05-13 16:46:31 -0500

    read projects from a file

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const PortfolioCard = (props) => (
-  <div className='w-full p-3 bg-indigo-200 text-gray-800 rounded shadow-md'>
-      <div className='text-2xl tracking-wider'>
-          {props.title}
+    <div className='w-full p-3 bg-indigo-200 text-gray-800 rounded shadow-md'>
+        <div className='text-xl font-semibold tracking-wider'>
+            {props.title}
         </div>
-      <div className='text-gray-700'>
-          {props.children}
+        <div className='text-gray-700'>
+            {props.children}
         </div>
     </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,51 +9,46 @@ import Skill from '../components/Skill';
 import Footer from '../components/Footer';
 import PortfolioCard from '../components/PortfolioCard';
 
-const Index = ({ skills }) => (
-  <>
-      <Head>
-          <title>Austin Crim - Web Developer Portfolio</title>
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+const Index = ({ skills, projects }) => (
+    <>
+        <Head>
+            <title>Austin Crim - Web Developer Portfolio</title>
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            <meta name='description' content='The portfolio of Austin Crim, a fullstack web developer'/>
         </Head>
-      <Nav />
-      <Hero />
-      <Section primary title='Tools I Have Used' id='skills'>
-          <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10'>
-              {skills.map((skill, index) => <Skill title={skill.title} image={skill.imagePath} alt={skill.alt} key={index} />)}
+        <Nav />
+        <Hero />
+        <Section primary title='Tools I Have Used' id='skills'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10'>
+                {skills.map((skill, index) => <Skill title={skill.title} image={skill.imagePath} alt={skill.alt} key={index} />)}
             </div>
         </Section>
-      <Section title='Things I Have Built' id='portfolio'>
-          <div className='grid gap-10'>
-              <PortfolioCard title='Lorem'>
-                  lorem ipsum bla slkdflka asld f; fdal
-                </PortfolioCard>
-              <PortfolioCard>
-                  Lorem
-                </PortfolioCard>
-              <PortfolioCard>
-                  Lorem
-                </PortfolioCard>
+        <Section title='Things I Have Built' id='portfolio'>
+            <div className='grid gap-10'>
+                {projects.map((project, index) => <PortfolioCard title={project.title} key={index}>{project.description}</PortfolioCard>)}
             </div>
         </Section>
-      <Section primary title='Want to Chat?' id='contact'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed
-          nemo in, dolor beatae facilis ratione vel! Quaerat dolor dicta
-          nisi explicabo consequatur deleniti dolorum iusto! Quis esse
-          explicabo ullam a? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Distinctio, neque! Porro quam cum tempore
-          illum inventore iure, sit provident doloremque optio sed
-          deserunt excepturi rerum facilis beatae labore dolorum
-          consequuntur.
+        <Section primary title='Want to Chat?' id='contact'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed
+            nemo in, dolor beatae facilis ratione vel! Quaerat dolor dicta
+            nisi explicabo consequatur deleniti dolorum iusto! Quis esse
+            explicabo ullam a? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Distinctio, neque! Porro quam cum tempore
+            illum inventore iure, sit provident doloremque optio sed
+            deserunt excepturi rerum facilis beatae labore dolorum
+            consequuntur.
         </Section>
-      <Footer>Designed and developed by Austin Crim</Footer>
+        <Footer>Designed and developed by Austin Crim</Footer>
     </>
 );
 
 export async function getStaticProps() {
     const { skills } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'data/skills.json')));
+    const { projects } = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'data/projects.json')));
     return {
         props: {
             skills,
+            projects
         },
     };
 }
