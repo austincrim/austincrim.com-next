7be39d3d8786dbe4f9faa3dff9a9a2faed2229a0
Austin Crim <crim.austin@principal.com>
2020-05-17 17:12:36 -0500

    still no inspiration for the contact section...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = (props) => (
-  <footer className='py-10 px-10 mx-auto text-center text-gray-600 text-sm bg-gray-300'>
+  <footer className='py-8 px-10 mx-auto text-center text-gray-600 text-sm bg-gray-300'>
       {props.children}
     </footer>
 );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,59 +5,59 @@ export default () => {
     const [expanded, setExpanded] = useState(false);
 
     return (
-      <nav>
-          <div className='flex justify-end bg-gray-200'>
-              <button
-                  className='m-4 md:hidden'
-                  onClick={() => setExpanded(!expanded)}
+        <nav>
+            <div className='flex justify-end bg-gray-200'>
+                <button
+                    className='m-4 md:hidden'
+                    onClick={() => setExpanded(!expanded)}
                 >
-                  <svg className='text-current text-gray-700' viewBox='0 0 100 80' width='40' height='40'>
-                      <rect
-                          rx='8'
-                          width='100'
-                          height='10'
+                    <svg className='text-current text-gray-700' viewBox='0 0 100 80' width='40' height='40'>
+                        <rect
+                            rx='8'
+                            width='100'
+                            height='10'
                         />
-                      <rect
-                          rx='8'
-                          y='30'
-                          width='100'
-                          height='10'
+                        <rect
+                            rx='8'
+                            y='30'
+                            width='100'
+                            height='10'
                         />
-                      <rect
-                          rx='8'
-                          y='60'
-                          width='100'
-                          height='10'
+                        <rect
+                            rx='8'
+                            y='60'
+                            width='100'
+                            height='10'
                         />
                     </svg>
                 </button>
             </div>
-          <div
-              className={`${expanded ? 'flex flex-col' : 'hidden'} md:flex md:flex-row justify-center py-2 w-full md:fixed text-gray-700 shadow-md bg-gray-100 z-10 border-t-4 border-indigo-500
+            <div
+                className={`${expanded ? 'flex flex-col' : 'hidden'} md:flex md:flex-row justify-center py-2 w-full md:fixed text-gray-700 shadow-md bg-gray-100 z-10 border-t-4 border-indigo-500
                 `}
             >
-              <Link href='/'>
-                  <a className='nav-item'>
-                      Home
+                <Link href='/'>
+                    <a className='nav-item'>
+                        Home
                     </a>
                 </Link>
-              <Link href='#skills'>
-                  <a className='nav-item'>
-                      Skills
+                <Link href='#skills'>
+                    <a className='nav-item'>
+                        Skills
                     </a>
                 </Link>
-              <Link href='#portfolio'>
-                  <a className='nav-item'>
-                      Portfolio
+                <Link href='#portfolio'>
+                    <a className='nav-item'>
+                        Portfolio
                     </a>
                 </Link>
-              <Link href='#contact'>
-                  <a className='nav-item'>
-                      Contact
+                <Link href='#contact'>
+                    <a className='nav-item'>
+                        Contact
                     </a>
                 </Link>
             </div>
-          {/* <Link href="/blog">
+            {/* <Link href="/blog">
           <a>
             <li>Blog</li>
           </a>
