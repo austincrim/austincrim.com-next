97b721f28cb7594a15adec915c203d42328c066a
Austin Crim <aust.crim@gmail.com>
2020-06-05 11:27:37 -0500

    file system based posts completed

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -2,8 +2,9 @@ import BlogNav from '../../components/BlogNav';
 import Prism from 'prismjs';
 import PrismJsx from 'prismjs/components/prism-jsx';
 import { useEffect } from 'react';
+import { getAllPostIds, getPostData } from '../../../lib/posts';
 
-const Post = (props) => {
+const Post = ({ postData }) => {
 
     useEffect(() => {
         Prism.highlightAll();
@@ -12,49 +13,40 @@ const Post = (props) => {
     return (
         <>
             <BlogNav />
-            <div className='mt-16 p-8 max-w-4xl mx-auto text-gray-800 bg-gray-200 rounded-md'>
-                <div className='flex justify-between items-center border-b border-indigo-200'>
-                    <h3 className='text-3xl text-teal-600 inline'>{props.post.title}</h3>
-                    <span className='text-gray-600'>
-                        {new Date(props.post.date).toLocaleDateString()}
-                    </span>
-                </div>
-                <div
-                    className='mt-8 font-serif text-left post-content'
-                    dangerouslySetInnerHTML={{ __html: props.post.content }}
-                />
+            <div className='my-16 p-10 max-w-4xl mx-auto text-gray-800 bg-gray-200 rounded-md'>
+                <article>
+                    <div className='flex justify-between items-center border-b border-indigo-200'>
+                        <h3 className='text-3xl text-teal-600 inline'>{postData.title}</h3>
+                        <span className='text-gray-600'>
+                            {new Date(postData.date).toLocaleDateString()}
+                        </span>
+                    </div>
+                    <div
+                        className='mt-8 text-lg font-sans text-left post-content'
+                        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+                    />
+                </article>
             </div>
         </>
     );
 }
 
 export async function getStaticPaths() {
-    const response = await fetch(
-        'https://austin-crim-blog-api.herokuapp.com/posts'
-    );
-    const posts = await response.json();
-
-    const paths = posts.map((post) => {
-        return {
-            params: {
-                id: post._id,
-            },
-        };
-    });
-    return { paths, fallback: false };
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    }
 }
 
 export async function getStaticProps({ params }) {
-    const { id } = params;
-    const response = await fetch(
-        `https://austin-crim-blog-api.herokuapp.com/posts/${id}`
-    );
-    const data = await response.json();
+    const postData = await getPostData(params.id);
+
     return {
         props: {
-            post: data,
-        },
-    };
+            postData
+        }
+    }
 }
 
 export default Post;
