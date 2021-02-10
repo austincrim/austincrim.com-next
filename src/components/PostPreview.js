9f8bad2b2f15b93787467cd6a49a5c9f3e13dbed
Austin Crim <crim.austin@principal.com>
2021-02-10 10:50:29 -0600

    clean up blog and posts, add new post

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 const PostPreview = ({ post }) => {
     return (
-        <div className='flex flex-col space-y-4 dark:text-gray-100'>
+        <div className='flex flex-col p-8 space-y-4 rounded shadow-md bg-gray-50 dark:bg-gray-800 dark:text-gray-100'>
             <div className='flex flex-col justify-between space-y-2 md:space-y-0 md:flex-row'>
                 <Link href={`/posts/${post.id}`}>
                     <a>
-                        <h4 className='text-xl font-bold tracking-wider transition-colors hover:text-gray-500 dark:hover:text-gray-300'>
+                        <h4 className='text-xl font-bold tracking-wider transition-colors hover:text-gray-500 dark:hover:text-gray-400'>
                             {post.title}
                         </h4>
                     </a>
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,7 @@ const Section = (props) => {
     return (
         <div
             id={props.id}
-            className='w-full py-12 leading-loose text-gray-700'
+            className={`w-full py-12 leading-loose text-gray-700 ${props.className}`}
         >
             <div className='max-w-6xl mx-auto'>
                 <h2 className='mb-6 text-3xl font-semibold text-blue-800 dark:text-blue-300 md:text-left'>
