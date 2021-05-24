b66d7bdd3306d64670c24405107f69ac432161b3
Austin Crim <crim.austin@principal.com>
2021-05-24 11:10:40 -0500

    preload font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,42 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
-      <Html lang="en-US">
+      <Html lang='en-US'>
         <Head>
           <meta
-            name="description"
-            content="The portfolio of Austin Crim, a fullstack web developer"
+            name='description'
+            content='The portfolio of Austin Crim, a fullstack web developer'
           />
-          <script type="text/javascript" src="/theme.js"></script>
+          <script type='text/javascript' src='/theme.js'></script>
+          <link
+            rel='preload'
+            href='/fonts/inter-var.ttf'
+            as='font'
+            type='font/ttf'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/rubik-var.ttf'
+            as='font'
+            type='font/ttf'
+            crossOrigin='anonymous'
+          />
         </Head>
-        <body className="text-base transition-colors bg-base">
+        <body className='text-base transition-colors bg-base'>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
