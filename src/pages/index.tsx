d4f4c96087d8cd79500fd7e3e9ab70838ab32072
Austin Crim <crim.austin@principal.com>
2021-06-06 15:55:09 -0500

    tidy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
-import * as React from 'react';
-import fs from 'fs';
-import path from 'path';
-import { getSortedPostsData } from '../lib/posts';
-import Hero from '../components/Hero';
-import Section from '../components/Section';
-import Footer from '../components/Footer';
-import ProjectCard from '../components/ProjectCard';
-import { LinkedInLogo, Mail } from '../components/Icons';
-import PostPreview from '../components/PostPreview';
-import Layout from '../components/Layout';
+import * as React from 'react'
+import fs from 'fs'
+import path from 'path'
+import { GetStaticProps } from 'next'
+import { getSortedPostsData } from '../lib/posts'
+import Hero from '../components/Hero'
+import Section from '../components/Section'
+import Footer from '../components/Footer'
+import ProjectCard from '../components/ProjectCard'
+import { LinkedInLogo, Mail } from '../components/Icons'
+import PostPreview from '../components/PostPreview'
+import Layout from '../components/Layout'
+import type { Post } from '@prisma/client'
 
-function Index({ projects, posts }) {
+export default function Index({
+  projects,
+  posts
+}: {
+  projects: any
+  posts: Array<Post>
+}) {
   return (
     <Layout>
       <main>
         <Hero />
-        <Section title="Things I Have Built" id="portfolio">
-          <div className="grid gap-10">
+        <Section title='Things I Have Built' id='portfolio'>
+          <div className='grid gap-10'>
             {projects.map((project) => (
               <ProjectCard project={project} key={project.title}>
                 {project.description}
@@ -24,8 +32,8 @@ function Index({ projects, posts }) {
             ))}
           </div>
         </Section>
-        <Section title="Things I Have Written" id="blog">
-          <ul className="flex flex-col mt-20 space-y-20">
+        <Section title='Things I Have Written' id='blog'>
+          <ul className='flex flex-col mt-20 space-y-20'>
             {posts.map((post) => (
               <li key={post.slug}>
                 <PostPreview post={post} />
@@ -33,32 +41,32 @@ function Index({ projects, posts }) {
             ))}
           </ul>
         </Section>
-        <Section title="Want to Chat?" id="contact">
-          <div className="grid grid-cols-2 mx-auto text-muted dark:text-gray-100">
-            <div className="p-4 text-center">
-              <div className="w-1/2 mx-auto text-lg font-semibold md:w-auto">
+        <Section title='Want to Chat?' id='contact'>
+          <div className='grid grid-cols-2 mx-auto text-muted dark:text-gray-100'>
+            <div className='p-4 text-center'>
+              <div className='w-1/2 mx-auto text-lg font-semibold md:w-auto'>
                 Email
-                <div className="w-8 h-8 mx-auto my-2 text-muted">
+                <div className='w-8 h-8 mx-auto my-2 text-muted'>
                   <Mail />
                 </div>
               </div>
               <a
-                href="mailto:aust.crim@gmail.com"
-                className="mt-1 transition-colors text-primary hover:text-secondary"
+                href='mailto:aust.crim@gmail.com'
+                className='mt-1 transition-colors text-primary hover:text-secondary'
               >
                 aust.crim@gmail.com
               </a>
             </div>
-            <div className="p-4 text-center">
-              <div className="w-1/2 mx-auto text-lg font-semibold md:w-auto">
+            <div className='p-4 text-center'>
+              <div className='w-1/2 mx-auto text-lg font-semibold md:w-auto'>
                 LinkedIn
-                <div className="w-8 h-8 mx-auto my-2 text-muted">
+                <div className='w-8 h-8 mx-auto my-2 text-muted'>
                   <LinkedInLogo />
                 </div>
               </div>
               <a
-                className="mt-1 transition-colors text-primary hover:text-secondary"
-                href="https://www.linkedin.com/in/austin-crim"
+                className='mt-1 transition-colors text-primary hover:text-secondary'
+                href='https://www.linkedin.com/in/austin-crim'
               >
                 Profile
               </a>
@@ -69,21 +77,19 @@ function Index({ projects, posts }) {
 
       <Footer>Designed and developed by Austin Crim</Footer>
     </Layout>
-  );
+  )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const { projects } = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), 'data/projects.json')).toString()
-  );
-  const posts = await getSortedPostsData();
-  
+  )
+  const posts = await getSortedPostsData()
+
   return {
     props: {
       projects,
       posts: posts.slice(0, 3)
     }
-  };
+  }
 }
-
-export default Index;
