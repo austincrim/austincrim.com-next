641310eebb8def1f2cea1c2df3d62c6c499d09b3
Austin Crim <crim.austin@principal.com>
2021-08-12 21:28:14 -0500

    center

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = (props) => (
-  <footer className="px-10 py-8 text-sm font-light tracking-wide text-muted">
-    {props.children}
-  </footer>
-);
-
-export default Footer;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = (props) => (
+  <footer className="px-10 py-8 text-sm font-light tracking-wide text-center text-muted">
+    {props.children}
+  </footer>
+);
+
+export default Footer;
