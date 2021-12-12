26898e03d952e4835b5ca2f7cdb12c08b8b76fe1
Austin Crim <aust.crim@gmail.com>
2021-12-12 11:09:48 -0600

    remove view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import * as React from 'react'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  React.useEffect(() => {
-    fetch(`/api/view?path=${router.asPath}`)
-  }, [router.asPath])
-
   return (
     <>
       <Head>
